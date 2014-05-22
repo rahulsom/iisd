569a2fb3dbaa721189abbdb8df36ef0c615e5da2
Rahul Somasunderam <rahul.som@gmail.com>
2014-05-21 22:28:59 -0700

    Updated to use latest SNAPSHOT

--- src/main/java/com/github/rahulsom/iisd/services/DocumentRepositoryImpl.java
+++ src/main/java/com/github/rahulsom/iisd/services/DocumentRepositoryImpl.java
@@ -7,6 +7,9 @@ import ihe.iti.xds_b._2007.RetrieveDocumentSetResponseType;
 import oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryResponseType;
 
 import javax.jws.WebService;
+import javax.xml.ws.AsyncHandler;
+import javax.xml.ws.Response;
+import java.util.concurrent.Future;
 
 @WebService(endpointInterface = "ihe.iti.xds_b._2007.DocumentRepositoryPortType")
 public class DocumentRepositoryImpl implements DocumentRepositoryPortType {
@@ -22,5 +25,25 @@ public class DocumentRepositoryImpl implements DocumentRepositoryPortType {
         withRegistryResponse(new RegistryResponseType().withStatus("SUCCESS")).
         withDocumentResponse()
         ;
+  }
+
+  @Override
+  public Response<RegistryResponseType> documentRepositoryProvideAndRegisterDocumentSetBAsync(ProvideAndRegisterDocumentSetRequestType body) {
+    return null;
+  }
+
+  @Override
+  public Future<?> documentRepositoryProvideAndRegisterDocumentSetBAsync(ProvideAndRegisterDocumentSetRequestType body, AsyncHandler<RegistryResponseType> asyncHandler) {
+    return null;
+  }
+
+  @Override
+  public Response<RetrieveDocumentSetResponseType> documentRepositoryRetrieveDocumentSetAsync(RetrieveDocumentSetRequestType body) {
+    return null;
+  }
+
+  @Override
+  public Future<?> documentRepositoryRetrieveDocumentSetAsync(RetrieveDocumentSetRequestType body, AsyncHandler<RetrieveDocumentSetResponseType> asyncHandler) {
+    return null;
   }
 }
