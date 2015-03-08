fd1f458a071b7d26a2f628963f9c4cfd9531eb43
Rahul Somasunderam <rahul.som@gmail.com>
2015-03-08 08:52:42 -0700

    Added failing test to detect netty from stack trace

--- src/test/java/com/github/rahulsom/iisds/test/HelloWorldServiceTest.java
+++ src/test/java/com/github/rahulsom/iisds/test/HelloWorldServiceTest.java
@@ -10,20 +10,79 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.xml.ws.AsyncHandler;
+import javax.xml.ws.Response;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"/service-definition-beans-test.xml"})
 public class HelloWorldServiceTest {
 
   @Autowired
   @Qualifier("repoClient")
-  private DocumentRepositoryPortType helloWorldClient;
+  private DocumentRepositoryPortType repoClient;
 
   @Test
-  public void helloWorldClientTest() {
+  public void helloWorldClientTest() throws ExecutionException, InterruptedException {
 
     RetrieveDocumentSetRequestType request = new RetrieveDocumentSetRequestType();
-    RetrieveDocumentSetResponseType response = helloWorldClient.documentRepositoryRetrieveDocumentSet(request);
+    RetrieveDocumentSetResponseType response = repoClient.documentRepositoryRetrieveDocumentSet(request);
     assert response.getRegistryResponse().getStatus().equals("SUCCESS");
+  }
+
+  @Test
+  public void helloWorldClientTestBulk() throws ExecutionException, InterruptedException {
+
+    RetrieveDocumentSetRequestType request = new RetrieveDocumentSetRequestType();
+    ArrayList<Future> futures = new ArrayList<Future>();
+    for (int i = 0; i < 200; i++) {
+      futures.add(repoClient.documentRepositoryRetrieveDocumentSetAsync(request, new AsyncHandler<RetrieveDocumentSetResponseType>() {
+        @Override
+        public void handleResponse(Response<RetrieveDocumentSetResponseType> res) {
+          RetrieveDocumentSetResponseType response = null;
+          try {
+            response = res.get();
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          } catch (ExecutionException e) {
+            e.printStackTrace();
+          }
+          assert response != null;
+          assert response.getRegistryResponse().getStatus().equals("SUCCESS");
+        }
+      }));
+    }
+
+    for (int i = 0; i < futures.size(); i++) {
+      futures.get(i).get();
+    }
+
+  }
+
+
+  @Test
+  public void helloWorldClientTest2() throws ExecutionException, InterruptedException {
+
+    RetrieveDocumentSetRequestType request = new RetrieveDocumentSetRequestType();
+    repoClient.documentRepositoryRetrieveDocumentSetAsync(request, new AsyncHandler<RetrieveDocumentSetResponseType>() {
+      @Override
+      public void handleResponse(Response<RetrieveDocumentSetResponseType> res) {
+        RetrieveDocumentSetResponseType response = null;
+        try {
+          response = res.get();
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        } catch (ExecutionException e) {
+          e.printStackTrace();
+        }
+        assert response != null;
+        assert response.getRegistryResponse().getStatus().equals("SUCCESS.2");
+      }
+    }).get();
+
+
   }
 
 }
